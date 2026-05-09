48d6bc38eb59572059e17128424d700b8498a797
srjuniorr <srjuniorr@users.noreply.github.com>
2026-05-08 22:41:37 -0600

    add js script

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// select the DOM elements for output
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// use the date object
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+lastModified.innerHTML = "Last Modification: " + document.lastModified;
